cec0471f688190ecb3eaf8417f426ef6bb5a9dac
Lenny the burger <tednboss@gmail.com>
2024-04-30 05:21:50 -0400

    add frdfs

--- src/shaders/common.glsl
+++ src/shaders/common.glsl
@@ -28,6 +28,7 @@ struct Material {
 	float roughness;
 };
 
+// This is not stored so we can be sloppy with how much data we use
 struct Triangle {
 	vec3 v0;
 	vec3 v1;
@@ -39,6 +40,11 @@ struct Triangle {
 	vec3 in_norm0;
 	vec3 in_norm1;
 	vec3 in_norm2;
+
+	// Relative axes
+	vec3 up;
+	vec3 right;
+	vec3 left;
 };
 
 // ========================= BUFFERS =========================
@@ -98,6 +104,11 @@ Triangle get_primitive(int index) {
 	prim.in_norm1 = normalize(cross(prim.v1 - prim.v2, prim.normal));
 	prim.in_norm2 = normalize(cross(prim.v2 - prim.v0, prim.normal));
 
+	// Calculate relative axes
+	prim.up = prim.normal;
+	prim.right = prim.in_norm0;
+	prim.left = cross(prim.in_norm0, prim.normal);
+
 	return prim;
 }
 
@@ -109,4 +120,37 @@ bool point_in_triangle(vec3 point, Triangle triangle) {
 	return plane_sdf(triangle.v0, triangle.in_norm0, point) >= 0.0 &&
 		   plane_sdf(triangle.v1, triangle.in_norm1, point) >= 0.0 &&
 		   plane_sdf(triangle.v2, triangle.in_norm2, point) >= 0.0;
+}
+
+// FIXME: doesnt work 
+vec4 debug_shader(vec3 point, Triangle surface) {
+	// convert to local coordinates of triangle
+	vec3 point_local = vec3(
+		plane_sdf(surface.mean, surface.right, point),
+		plane_sdf(surface.mean, surface.up, point),
+		plane_sdf(surface.mean, surface.normal, point)
+	);
+
+	return vec4(DEBUG_COLOR * 
+		(sin(point_local.y * GRID_SHADER_SIZE) > cos(point_local.z * GRID_SHADER_SIZE) ? 1.0 : 0.0),
+		1.0);
+}
+
+// Gaussian approximation for the free ray distribution function of 
+// a perfectly lambertian BRDF.
+float FRDF_gauss(float smple, float mean, float stdev) {
+	return exp(
+		-(pow(smple - mean, 2.0) / (2.0 * pow(stdev, 2.0)))
+	);
+}
+
+// Gaussian approximation for the FRDF but different to make it look more 
+// accurate (thanks stole) see: https://www.desmos.com/calculator/8k36ti44zx
+float FRDF_gauss_adj(float smple, float mean, float stdev) {
+	// What a and a2 here are isnt important they are just random equations
+	// that happen to make the function look more accurate.
+	float a  = (1.0 / stdev) * (smple - mean);
+	float a2 = (pow(abs(a), 3)) / (10.0 + pow(a, 4));
+
+	return FRDF_gauss(smple, mean, stdev) + a2;
 }
--- src/shaders/fragment.frag
+++ src/shaders/fragment.frag
@@ -10,6 +10,17 @@ uniform int debug_id;
 
 void main()
 {
+	Triangle tri = get_primitive(gl_PrimitiveID);
+
+	// Debugging
+	if (gl_PrimitiveID == debug_id) {
+		// Set to a purple grid for debugging
+		FragColor = vec4(DEBUG_COLOR * 
+			(sin(worldPos.x * GRID_SHADER_SIZE) > cos(worldPos.z * GRID_SHADER_SIZE) ? 1.0 : 0.0),
+			1.0);
+		return;
+	}
+
 	// get material from the ssbo
 	Material mat = material_buffer[gl_PrimitiveID];
 	Lightmap lm = lightmaps[gl_PrimitiveID];
@@ -22,22 +33,13 @@ void main()
 
 	depth *= 10.0f;
 
-	Triangle test_prim = get_primitive(979);
-
-	bool is_within = point_in_triangle(worldPos, test_prim);
-
-	float is_within_mult = is_within ? 1.0 : 0.0;
-
-	// Debugging
-	if (gl_PrimitiveID == debug_id) {
-		// Set to a purple grid for debugging
-		FragColor = vec4(DEBUG_COLOR * 
-			(sin(worldPos.x * GRID_SHADER_SIZE) > cos(worldPos.z * GRID_SHADER_SIZE) ? 1.0 : 0.0),
-			1.0);
-		return;
-	}
-
 	float debug_dot = mat.emissive_strength;
 
-    FragColor = vec4(mat_col * depth * debug_dot * is_within_mult, 1.0);
+    //FragColor = vec4(mat_col * depth * debug_dot * is_within_mult, 1.0);
+
+	vec3 tempOrg = vec3(0.0, 0.0, 0.0);
+
+	float testval = FRDF_gauss_adj(distance(worldPos, tempOrg), 0, 1);
+
+	FragColor = vec4(vec3(testval), 1.0);
 }
